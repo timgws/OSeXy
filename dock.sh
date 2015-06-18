1b3808029a5c6f6de7d05ec5eaa27f949aaf9304
Tim Groeneveld <tim@timg.ws>
2015-06-18 13:46:16 +1000

    Clear the Dock, add browsers, github, iterm, phpstorm & vmware

--- dock.sh
+++ dock.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
+clear_dock () {
+    defaults write com.apple.dock persistent-apps -array
+}
+
 dock () {
-    DIRECTORY="/Users/`whoami`/$1";
+    DIRECTORY="/Users/`whoami`/Applications/$1.app";
     XML="<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>${DIRECTORY}</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>"
     defaults write com.apple.dock persistent-apps -array-add "$XML"
 }
@@ -10,18 +14,15 @@ restart_dock() {
     killall -SIGHUP Dock
 }
 
-dock "Cyberduck.app"
-dock "Dash.app"
-dock "Firefox.app"
-dock "Google Chrome.app"
-dock "GitHub.app"
-dock "HandBrake.app"
-dock "Opera.app"
-dock "RecordIt.app"
-dock "VMWare Fusion.app"
-dock "Spotify.app"
-dock "Skype.app"
-dock "Slack.app"
-dock "PhpStorm.app"
-
-
+clear_dock
+dock "Google Chrome"
+dock "Firefox"
+dock "GitHub"
+dock "iTerm"
+dock "Dash"
+dock "GitHub"
+dock "Skype"
+dock "Spotify"
+dock "PhpStorm"
+dock "VMWare Fusion"
+restart_dock
