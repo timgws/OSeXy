9d1a3a1d4139ebb950945b9437948bdbfe50296e
Tim Groeneveld <tim@timg.ws>
2015-06-19 17:06:56 +1000

    Put Sequal Pro and MAMP into the Dock.

--- dock.sh
+++ dock.sh
@@ -10,6 +10,12 @@ dock () {
     defaults write com.apple.dock persistent-apps -array-add "$XML"
 }
 
+full_dock () {
+    DIRECTORY="$1.app";
+    XML="<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>${DIRECTORY}</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>"
+    defaults write com.apple.dock persistent-apps -array-add "$XML"
+}
+
 restart_dock() {
     killall -SIGHUP Dock
 }
@@ -18,9 +24,10 @@ clear_dock
 dock "Google Chrome"
 dock "Firefox"
 dock "GitHub"
+full_dock "/Applications/MAMP/Sequel Pro"
 dock "iTerm"
 dock "Dash"
-dock "GitHub"
+full_dock "/Applications/MAMP/MAMP"
 dock "Skype"
 dock "Spotify"
 dock "PhpStorm"
